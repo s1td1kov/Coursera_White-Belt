3ee7fe140d1f6ba7efb126f014f9da421b36d6c2
RusS1103 <37127343+RusS1103@users.noreply.github.com>
2018-12-14 22:28:27 +0300

    Create minimalstring.cpp

---
+++ minimalstring.cpp
@@ -0,0 +1,26 @@
+//============================================================================
+// Name        : Minimal String
+// Author      : Ruslan Sitdikov
+// Version     : C++17
+// Copyright   : Your copyright notice
+//============================================================================
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+  string a, b, c;
+  cin >> a >> b >> c;
+  if (a <= b && a <= c) {
+    cout << a;
+  } 
+  else if (b <= a && b <= c) {
+    cout << b;
+  }
+  else {
+    cout << c;
+  }
+  return 0;
+}
