d9bd50d56a080f9b487299b4fba1d157ec1758ba
RusS1103 <37127343+RusS1103@users.noreply.github.com>
2018-12-14 22:04:18 +0300

    Update a+b.cpp

--- a+b.cpp
+++ a+b.cpp
@@ -1,10 +1,14 @@
-#include <iostream>
+//============================================================================
+// Name        : a+b
+// Author      : Ruslan Sitdikov
+// Version     : C++17
+// Copyright   : Your copyright notice
+//============================================================================
 
+#include <iostream>
 using namespace std;
 
-int main()
-
-{
+int main() {
 	int a, b;
 	cin >> a >> b;
 	int c = a + b;
