eb6afdbf679add1c9716bc346328e9a8321ca1ae
RusS1103 <37127343+RusS1103@users.noreply.github.com>
2018-12-15 23:02:02 +0300

    Create BinaryNumbers.cpp

---
+++ BinaryNumbers.cpp
@@ -0,0 +1,29 @@
+//
+//  BinaryNumbers.cpp
+//  CourseraProjects White Belt Week1
+//  Created by Ruslan on 15/12/2018.
+//  Copyright © 2018 Ruslan. All rights reserved.
+//
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    vector <int> a;
+    while(n > 0) {
+        int temp = n % 2;
+        a.push_back(temp);
+        n /= 2;
+    }
+    reverse(begin(a), end(a));
+    for(auto now : a) {
+        cout << now;
+    }
+    cout << endl;
+    return 0;
+}
