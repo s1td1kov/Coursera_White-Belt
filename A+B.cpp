522e32a818719c64c95b812f00c5d574b84fb8cd
RusS1103 <37127343+RusS1103@users.noreply.github.com>
2018-12-15 22:49:01 +0300

    Update and rename a+b.cpp to A+B.cpp

---
+++ A+B.cpp
@@ -0,0 +1,17 @@
+//
+//  GCD.cpp
+//  CourseraProjects White Belt Week1
+//  Created by Ruslan on 15/12/2018.
+//  Copyright © 2018 Ruslan. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a, b;
+	cin >> a >> b;
+	int c = a + b;
+	cout << c;
+	return 0;
+}
