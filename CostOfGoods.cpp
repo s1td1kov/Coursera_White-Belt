651a74424a9131734a207f653ecf79daf5cab456
RusS1103 <37127343+RusS1103@users.noreply.github.com>
2018-12-15 23:14:11 +0300

    Create CostOfGoods.cpp

---
+++ CostOfGoods.cpp
@@ -0,0 +1,23 @@
+//
+//  CostOfGoods.cpp
+//  CourseraProjects White Belt Week1
+//  Created by Ruslan on 15/12/2018.
+//  Copyright © 2018 Ruslan. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    double n, a, b, x, y;
+    cin >> n >> a >> b >> x >> y;
+    if (n > b) {
+        n *= (1 - y / 100);
+    } else if (n > a) {
+        n *= (1 - x / 100);
+    }
+    cout << n;
+    
+    return 0;
+}
