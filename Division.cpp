5c9417bf2fe9fcdf6deaef5b13c8a9cebf5807f1
RusS1103 <37127343+RusS1103@users.noreply.github.com>
2018-12-15 23:12:50 +0300

    Create Division.cpp

---
+++ Division.cpp
@@ -0,0 +1,24 @@
+//
+//  Division.cpp
+//  CourseraProjects White Belt Week1
+//  Created by Ruslan on 15/12/2018.
+//  Copyright © 2018 Ruslan. All rights reserved.
+//
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    if(b == 0) {
+        cout << "Impossible" << endl;
+    }
+    else {
+        cout << a / b << endl;
+    }
+    return 0;
+}
