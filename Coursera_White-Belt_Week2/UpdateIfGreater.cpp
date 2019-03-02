ad82541c27a820044dd6d6ff091b0b7579137f85
Ruslan <37127343+RusS1103@users.noreply.github.com>
2019-03-02 23:41:41 +0300

    Create UpdateIfGreater.cpp

---
+++ Coursera_White-Belt_Week2/UpdateIfGreater.cpp
@@ -0,0 +1,12 @@
+//
+//  UpdateIfGreater.cpp
+//  CourseraProjects White Belt Week2
+//  Created by Ruslan Sitdikov on 16/12/2018.
+//  Copyright © 2018 Ruslan. All rights reserved.
+//
+
+void UpdateIfGreater(int& first, int& second) {
+    if(first > second) {
+        second  = first;
+    }
+}
