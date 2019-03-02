72e6bd038cbf4d35d706d5448918f7589af4fcce
Ruslan <37127343+RusS1103@users.noreply.github.com>
2019-03-02 23:44:49 +0300

    Create Incognizable.cpp

---
+++ Coursera_White-Belt_Week3/Incognizable.cpp
@@ -0,0 +1,17 @@
+//
+//  Incognizable.cpp
+//  CourseraProjects White Belt Week3
+//  Created by Ruslan Sitdikov on 26/01/2019.
+//  Copyright © 2019 Ruslan Sitdikov. All rights reserved.
+//
+
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <string>
+#include <map>
+
+struct Incognizable {
+  int x = 0;
+  int y = 0;
+  };
