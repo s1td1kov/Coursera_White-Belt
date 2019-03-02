a0eac7566aee5a5bda2d4f1cddad0ee1393249c1
Ruslan <37127343+RusS1103@users.noreply.github.com>
2019-03-02 23:54:06 +0300

    Create ReversibleFunction.cpp

---
+++ Coursera_White-Belt_Week4/ReversibleFunction.cpp
@@ -0,0 +1,56 @@
+//
+//  ReversibleFunction.cpp
+//  CourseraProjects White Belt Week4
+//  Created by Ruslan Sitdikov on 08/02/2019.
+//  Copyright © 2019 Ruslan Sitdikov. All rights reserved.
+//
+
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+class FunctionPart {
+public:
+    FunctionPart(char new_operation, double new_value) {
+        operation = new_operation;
+        value = new_value;
+    }
+    double Apply(double source_value) const {
+        if (operation == '+') {
+            return source_value + value;
+        } else {  // operation == '-'
+            return source_value - value;
+        }
+    }
+    void Invert() {
+        if (operation == '+') {
+            operation = '-';
+        } else {  // operation == '-'
+            operation = '+';
+        }
+    }
+private:
+    char operation;
+    double value;
+};
+
+class Function {
+public:
+    void AddPart(char operation, double value) {
+        parts.push_back({operation, value});
+    }
+    double Apply(double value) const {
+        for (const FunctionPart& part : parts) {
+            value = part.Apply(value);
+        }
+        return value;
+    }
+    void Invert() {
+        for (FunctionPart& part : parts) {
+            part.Invert();
+        }
+        reverse(begin(parts), end(parts));
+    }
+private:
+    vector<FunctionPart> parts;
+};
