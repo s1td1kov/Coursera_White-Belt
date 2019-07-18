677c2ef1182c34f20043ab748c77a2fe53fa7e5a
Ruslan <37127343+RusS1103@users.noreply.github.com>
2019-07-18 14:25:31 +0500

    Update and rename Coursera_White-Belt_Week1/CostOfGoods.cpp to Неделя 1/Расчёт стоимости товара.cpp

--- Неделя 1/Расчёт стоимости товара.cpp
+++ Неделя 1/Расчёт стоимости товара.cpp
@@ -1,10 +1,3 @@
-//
-//  CostOfGoods.cpp
-//  CourseraProjects White Belt Week1
-//  Created by Ruslan Sitdikov on 15/12/2018.
-//  Copyright © 2018 Ruslan Sitdikov. All rights reserved.
-//
-
 #include <iostream>
 
 using namespace std;
